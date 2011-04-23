6003e15880a48a1ec67e0b03887f3fd144521d5b
Stuart Moore <stuart.moore@gmail.com>
2011-04-23 01:54:27 -0700

    NRPE restart method changed depending upon client OS. This is only temporary untill "/etc/init.d/nrpe stop" is fixed

--- update_nrpe.sh
+++ update_nrpe.sh
@@ -2,6 +2,8 @@
 
 # v1.0 stuart.moore@monitisegroup.com
 #	+ Pushes NRPE to the defined host.
+# v1.1 stuart.moore@monitisegroup.com
+#      + NRPE restart method changed depending upon client OS. This is only temporary untill "/etc/init.d/nrpe stop" is fixed
 
 usage()
 {
@@ -37,14 +39,27 @@ if [ -z $FQDN ]; then
 	usage
 	exit 1
 else
+#
+# FIXME! - The below command runs 3 ssh commands, when one will do. Time to learn proper string parsing ...
+#
 	HOST_OS=`ssh -l root $FQDN uname`
 	HOST_PLATFORM=`ssh -l root $FQDN uname -i`
 	ZONENAME='ssh -l root $FQDN zonename'
 	scp /usr/local/nagios/scripts/nrpe.$HOST_OS.$HOST_PLATFORM.tar.gz root@$FQDN:/opt
 	ssh root@$FQDN "rm /etc/init.d/nrpe"
 	ssh root@$FQDN "cd /opt ; gunzip nrpe.$HOST_OS.$HOST_PLATFORM.tar.gz; tar xf nrpe.$HOST_OS.$HOST_PLATFORM.tar ; cd nrpe ; ./nrpe.install ; cd .. ; rm nrpe.$HOST_OS.$HOST_PLATFORM.tar"
-	ssh root@$FQDN "pkill -z $ZONENAME nrpe"
-	ssh root@$FQDN "/etc/init.d/nrpe start"
+
+case $HOST_OS in
+  SunOS)
+    ssh root@$FQDN "pkill -z $ZONENAME nrpe"
+    ;;
+  Linux)
+    ssh root@$FQDN killall nrpe"
+    ;;
+  *)
+    echo -e "Unknown OS, unable to safely stop NRPE. Carrying on anyway ...
+    ;;
+esac
 	/etc/init.d/nagios reload
 fi
 
